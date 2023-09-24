ff8663f12c8f8275859f393cfa611c76ab590a00
Charlie Hoy <hoyc1@cardiff.ac.uk>
2023-09-24 17:12:39 +0100

    comment out to_excel

--- script_without_read_excel.py
+++ script_without_read_excel.py
@@ -53,7 +53,7 @@ for num, name in enumerate(unique_names):
     new_df["Number of Academic Sanctions"][num] = np.sum(_type == "Academic Sanction")
     new_df["Total Value"][num] = np.sum(df["Points value of sanction"][bool].astype(np.float32))
 
-new_df.to_excel(opts.output_excel, index=False)
+#new_df.to_excel(opts.output_excel, index=False)
 new_df.to_html(buf=opts.output_html, index=False)
 """
 for year in np.unique(df["Year"]):
